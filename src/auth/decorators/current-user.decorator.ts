abd76c1a2b69ef01adb6eca6581fd364e61daf45
paponobm <ashfaqurpaponobm@gmail.com>
2026-09-26 17:13:56 +0600

    switch feature implement

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -4,6 +4,7 @@ import { JwtPayload } from '../types/jwt-payload.type';
 export const CurrentUser = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext): JwtPayload => {
     const request = ctx.switchToHttp().getRequest();
+    
     return request.user;
   },
 );
